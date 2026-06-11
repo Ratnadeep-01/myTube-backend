43af5cf026d13c8f94d055cd1a052dd37ef2ec62
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-06-11 19:15:07 +0530

    add getUserChannelProfile endpoint to fetch user channel details and subscription status

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -326,4 +326,92 @@ const updateUserCoverImage= asyncHandler(async(req, res) => {
     return res.status(200).json(new ApiResponse(200, {user}, "coverImage uploaded successfully"))
 })
 
-export {registerUser, loginUser, logoutUser, refreshAccessToken, generateAccessAndRefreshToken, changePassword, forgotPassword, getCurrentUser, updateUserProfile, updateUserAvatar, updateUserCoverImage};
+const getUserChannelProfile = asyncHandler (async(req,res) => {
+    const {username} = req.params;
+
+    if(!username?.trim()){
+        throw new ApiError(400, {}, "username is missing");
+    }
+
+    const currentUserId = req.user?._id ? new mongoose.Types.ObjectId(req.user._id) : null;
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribeTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount :{
+                    $size: "$subscribers"
+                },
+                subscribeToCount: {
+                    $size: "$subscribeTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: currentUserId 
+                            ? { $in: [ currentUserId, "$subscribers.subscriber" ] } 
+                            : false, // If logged out, immediately evaluate to false without confusing MongoDB
+                        then : true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                subscribeToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "user channel fetched successfully")
+    )
+})
+
+export {
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    generateAccessAndRefreshToken, 
+    changePassword, 
+    forgotPassword, 
+    getCurrentUser, 
+    updateUserProfile, 
+    updateUserAvatar, 
+    updateUserCoverImage,
+    getUserChannelProfile
+};
