b0c8c08ae97158470971b87bb0a1845f86d18a52
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-06-18 19:38:24 +0530

    add dashboard controller with channel stats and videos retrieval

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+import {Video} from "../models/video.model.js"
+import {Subscription} from "../models/subscription.model.js"
+import {Like} from "../models/like.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const { channelId } = req.params
+    const totalVideos = await Video.countDocuments({ channel: channelId })
+    const totalSubscribers = await Subscription.countDocuments({ channel: channelId })
+    const totalLikes = await Like.countDocuments({ channel: channelId })
+
+    return res.status(200).json(new ApiResponse(200, { totalVideos, totalSubscribers, totalLikes }, "Channel stats fetched successfully"))
+})
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+    const { channelId } = req.params
+    const videos = await Video.find({ channel: channelId })
+    return res.status(200).json(new ApiResponse(200, videos, "Videos fetched successfully"))
+})
+
+export {
+    getChannelStats, 
+    getChannelVideos
+    }
