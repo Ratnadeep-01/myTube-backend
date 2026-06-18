6340494bc90fd6071447b374e00ef3133adfb91f
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-06-18 19:37:10 +0530

    add comment controller with CRUD operations for video comments

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose"
+import {Comment} from "../models/comment.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+    const comments = await Comment.find({video: videoId})
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .populate("user", "name avatar")
+    const totalComments = await Comment.countDocuments({video: videoId})
+    return res.status(200).json(new ApiResponse(200, {comments, totalComments}, "Comments fetched successfully"))
+
+})
+
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+    const {videoId} = req.params
+    const {text} = req.body
+    const comment = await Comment.create({video: videoId, user: req.user._id, text})
+    return res.status(201).json(new ApiResponse(201, comment, "Comment added successfully"))
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+    const {commentId} = req.params
+    const {text} = req.body
+    const comment = await Comment.findOneAndUpdate({_id: commentId, user: req.user._id}, {text}, {new: true})
+    if (!comment) {
+        throw new ApiError(404, "Comment not found or you are not the owner of the comment")
+    }
+    return res.status(200).json(new ApiResponse(200, comment, "Comment updated successfully"))
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+    const {commentId} = req.params
+    const comment = await Comment.findOneAndDelete({_id: commentId, user: req.user._id})
+    if (!comment) {
+        throw new ApiError(404, "Comment not found or you are not the owner of the comment")
+    }
+    return res.status(200).json(new ApiResponse(200, null, "Comment deleted successfully"))
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+     deleteComment
+    }
