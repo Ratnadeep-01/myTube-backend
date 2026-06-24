35e6da304fc5669bd2bed5a8fe86623b35522b18
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-06-24 15:14:39 +0530

    fix: update comment controller to use 'owner' field for consistency in comment operations

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -11,7 +11,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const comments = await Comment.find({video: videoId})
         .skip((page - 1) * limit)
         .limit(limit)
-        .populate("user", "name avatar")
+        .populate("owner", "name avatar")
     const totalComments = await Comment.countDocuments({video: videoId})
     return res.status(200).json(new ApiResponse(200, {comments, totalComments}, "Comments fetched successfully"))
 
@@ -21,7 +21,7 @@ const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
     const {videoId} = req.params
     const {text} = req.body
-    const comment = await Comment.create({video: videoId, user: req.user._id, text})
+    const comment = await Comment.create({video: videoId, owner: req.user._id, content: text})
     return res.status(201).json(new ApiResponse(201, comment, "Comment added successfully"))
 })
 
@@ -29,7 +29,7 @@ const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
     const {commentId} = req.params
     const {text} = req.body
-    const comment = await Comment.findOneAndUpdate({_id: commentId, user: req.user._id}, {text}, {new: true})
+    const comment = await Comment.findOneAndUpdate({_id: commentId, owner: req.user._id}, {content: text}, {new: true})
     if (!comment) {
         throw new ApiError(404, "Comment not found or you are not the owner of the comment")
     }
@@ -39,7 +39,7 @@ const updateComment = asyncHandler(async (req, res) => {
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
     const {commentId} = req.params
-    const comment = await Comment.findOneAndDelete({_id: commentId, user: req.user._id})
+    const comment = await Comment.findOneAndDelete({_id: commentId, owner: req.user._id})
     if (!comment) {
         throw new ApiError(404, "Comment not found or you are not the owner of the comment")
     }
