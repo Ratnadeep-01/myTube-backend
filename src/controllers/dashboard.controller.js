b33148ad3c5165d3973dbdf96b4613399f148c43
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-06-24 15:14:28 +0530

    fix: update dashboard controller to use 'owner' field for fetching channel stats and videos

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -9,9 +9,9 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
     const { channelId } = req.params
-    const totalVideos = await Video.countDocuments({ channel: channelId })
-    const totalSubscribers = await Subscription.countDocuments({ channel: channelId })
-    const totalLikes = await Like.countDocuments({ channel: channelId })
+    const totalVideos = await Video.countDocuments({ owner: channelId })
+    const totalSubscribers = await Subscription.countDocuments({ owner: channelId })
+    const totalLikes = await Like.countDocuments({ owner: channelId })
 
     return res.status(200).json(new ApiResponse(200, { totalVideos, totalSubscribers, totalLikes }, "Channel stats fetched successfully"))
 })
@@ -19,7 +19,7 @@ const getChannelStats = asyncHandler(async (req, res) => {
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
     const { channelId } = req.params
-    const videos = await Video.find({ channel: channelId })
+    const videos = await Video.find({ owner: channelId })
     return res.status(200).json(new ApiResponse(200, videos, "Videos fetched successfully"))
 })
 
